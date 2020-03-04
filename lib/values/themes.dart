5c0a8cb3fb91caf3db26a598ae9f9dd2106277ca
Flavio Traverso <flavio@MacBook-Pro-di-Flavio.local>
2020-03-04 17:38:19 +0100

    Values updated

---
+++ lib/values/themes.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'values.dart';
+
+class AppThemes {
+	AppThemes._();
+
+	static final ThemeData lightTheme = ThemeData(
+		scaffoldBackgroundColor: AppColors.primaryBackground,
+		brightness: Brightness.light);
+
+}

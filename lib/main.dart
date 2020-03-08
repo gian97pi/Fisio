d8a9d156811eb2ed43d38e6da9a4a9da728580cf
DiTo97 <fede97.minutoli@gmail.com>
2020-03-08 19:43:15 +0100

    Flutter setup

--- lib/main.dart
+++ lib/main.dart
@@ -12,12 +12,12 @@ import 'package:flutter/material.dart';
 void main() => runApp(App());
 
 class App extends StatelessWidget {
-  
-  @override
-  Widget build(BuildContext context) {
-  
-    return MaterialApp(
-      home: Riepilogo1Widget(),
-    );
-  }
+
+	@override
+	Widget build(BuildContext context) {
+
+		return MaterialApp(
+			home: Riepilogo1Widget(),
+		);
+	}
 }
